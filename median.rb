4add777b852d34d18ab98c22f2e4baf3bd097c99
Patrick <pat.rodriguez108@gmail.com>
2017-12-06 14:15:54 -0600

    Able to find median if total amount of numbers are odd

--- median.rb
+++ median.rb
@@ -1,7 +1,15 @@
 def find_median_sorted_arrays(nums1, nums2)
 	all_nums = nums1 + nums2
 	all_nums = all_nums.sort
-	lowbound = 0
-	highbound = all_nums.length - 1
-	
+	midpoint_index = all_nums.length / 2
+	if midpoint_index.odd?
+		"The median is #{all_nums[midpoint_index].to_f}"
+	else
+
+	end
 end
+
+nums1 = [1, 3]
+nums2 = [2]
+
+p find_median_sorted_arrays(nums1, nums2)
