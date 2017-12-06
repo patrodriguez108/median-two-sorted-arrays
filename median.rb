f50f767f4ba8df088228cfde39f801c371622a1a
Patrick <pat.rodriguez108@gmail.com>
2017-12-06 14:27:50 -0600

    Find median value for if amount of total numbers is even

--- median.rb
+++ median.rb
@@ -5,7 +5,8 @@ def find_median_sorted_arrays(nums1, nums2)
 	if midpoint_index.odd?
 		"The median is #{all_nums[midpoint_index].to_f}"
 	else
-
+		median_value = (all_nums[midpoint_index - 1] + all_nums[midpoint_index]).to_f / 2
+		"The median is (#{all_nums[midpoint_index - 1]} + #{all_nums[midpoint_index]}) / 2 = #{median_value}"
 	end
 end
 
@@ -13,3 +14,8 @@ nums1 = [1, 3]
 nums2 = [2]
 
 p find_median_sorted_arrays(nums1, nums2)
+
+nooms1 = [1, 2]
+nooms2 = [3, 4]
+
+p find_median_sorted_arrays(nooms1, nooms2)
